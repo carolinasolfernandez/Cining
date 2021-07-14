f143f7b710be0ff9d86a5ca00dbdc096f7c5746b
Carolina Sol Fernández <carolinasolfernandez@gmail.com>
2021-07-13 21:08:03 -0300

    firestore - google auth - storage - location

--- app/src/main/java/com/csf/cining/fragments/cines/CreateFragment.kt
+++ app/src/main/java/com/csf/cining/fragments/cines/CreateFragment.kt
@@ -1,25 +1,42 @@
 package com.csf.cining.fragments.cines
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.FrameLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.csf.cining.R
 import com.csf.cining.database.AppDatabase
 import com.csf.cining.entities.Cine
+import com.google.android.gms.tasks.Tasks
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
+import java.sql.Timestamp
+import java.util.*
 
 
 class CreateFragment : Fragment() {
-
+    private val REQUEST_IMAGE_CAPTURE = 1
     lateinit var v: View
 
     private lateinit var name: EditText
@@ -27,8 +44,10 @@ class CreateFragment : Fragment() {
     private lateinit var website: EditText
     private lateinit var latitude: EditText
     private lateinit var longitude: EditText
-    private lateinit var image: EditText
+    private lateinit var image: ImageView
     private lateinit var button: Button
+
+    private lateinit var cloudDb: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,91 +55,149 @@ class CreateFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_cine_create, container, false)
+        cloudDb = FirebaseFirestore.getInstance()
         return v
     }
 
     override fun onStart() {
         super.onStart()
-        var cine: Cine? = null
+        //var cine: Cine? = null
 
         val cineId = DetailFragmentArgs.fromBundle(requireArguments()).comCsfCiningEntitiesCineId
-        if (cineId != 0) {
-            val db = AppDatabase.getAppDatabase(v.context)!!
-            val cineDao = db.cineDao()
-            cine = cineDao.getCineById(cineId)
-        }
 
         name = v.findViewById(R.id.txtCineName)
         address = v.findViewById(R.id.txtCineAddress)
         website = v.findViewById(R.id.txtCineWebsite)
         latitude = v.findViewById(R.id.txtCineLatitude)
         longitude = v.findViewById(R.id.txtCineLongitude)
-        image = v.findViewById(R.id.txtCineImage)
         button = v.findViewById(R.id.create_button)
+        image = v.findViewById(R.id.image)
 
-        if (cine != null) {
-            name.setText(cine.name)
-            address.setText(cine.address)
-            website.setText(cine.website)
-            latitude.setText("" + cine.latitude)
-            longitude.setText("" + cine.longitude)
-            image.setText(cine.image)
+        image.setOnClickListener() {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
         }
 
-        val db = AppDatabase.getAppDatabase(v.context)!!
-        val cineDao = db.cineDao()
+        if (cineId != "") {
+            //val db = AppDatabase.getAppDatabase(v.context)!!
+            //val cineDao = db.cineDao()
+            //cine = cineDao.getCineById(cineId)
+            edit(cineId)
+        } else {
+            create()
+        }
 
-        button = v.findViewById(R.id.create_button)
+        //val db = AppDatabase.getAppDatabase(v.context)!!
+        //val cineDao = db.cineDao()
+
+
+    }
+
+    private fun edit(cineId: String) {
+        cloudDb.collection("cines").document(cineId)
+            .get().addOnSuccessListener { documentSnapshot ->
+                documentSnapshot?.toObject(Cine::class.java)?.let { cine ->
+                    run {
+                        name.setText(cine.name)
+                        address.setText(cine.address)
+                        website.setText(cine.website)
+                        latitude.setText("" + cine.latlng.latitude)
+                        longitude.setText("" + cine.latlng.longitude)
+                        Glide.with(v.context).load(cine.image).into(image);
+                        image.tag = cine.image
+                        if (cine.image != null && cine.image != "") {
+                            image.setBackgroundResource(android.R.color.transparent)
+                        }
+
+                        button.setOnClickListener() {
+                            if (checkFields()) {
+                                var latitudeStr = latitude.text.toString()
+                                var longitudeStr = longitude.text.toString()
+                                if (latitudeStr == "" || longitudeStr == "") {
+                                    latitudeStr = "0"
+                                    longitudeStr = "0"
+                                }
+                                cine.name = name.text.toString()
+                                cine.address = address.text.toString()
+                                cine.website = website.text.toString()
+                                cine.latlng =
+                                    GeoPoint(latitudeStr.toDouble(), longitudeStr.toDouble())
+                                cine.image = image.tag.toString()
+                                //cineDao.updateCine(cine)
+                                cloudDb.collection("cines").document(cine.id).set(cine)
+
+                                Snackbar.make(v, "Cine ${name.text} saved!", Snackbar.LENGTH_LONG)
+                                    .show()
+                                name.text.clear()
+                                address.text.clear()
+                                website.text.clear()
+                                latitude.text.clear()
+                                longitude.text.clear()
+                                image.tag = ""
+
+                                val action = CreateFragmentDirections.actionCineCreateToCineList()
+                                v.findNavController().navigate(action)
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun create() {
         button.setOnClickListener() {
-            if (isComplete(name, getString(R.string.incomplete_name)) && (
-                        isComplete(address, getString(R.string.incomplete_address)) || (
-                                isComplete(latitude, getString(R.string.incomplete_coordinates)) &&
-                                        isComplete(
-                                            longitude,
-                                            getString(R.string.incomplete_coordinates)
-                                        )
-                                )
-                        )
-            ) {
+            if (checkFields()) {
                 var latitudeStr = latitude.text.toString()
                 var longitudeStr = longitude.text.toString()
                 if (latitudeStr == "" || longitudeStr == "") {
                     latitudeStr = "0"
                     longitudeStr = "0"
                 }
-                if (cine != null) {
-                    cine.name = name.text.toString()
-                    cine.address = address.text.toString()
-                    cine.website = website.text.toString()
-                    cine.latitude = latitudeStr.toDouble()
-                    cine.longitude = longitudeStr.toDouble()
-                    cine.image = image.text.toString()
-                    cineDao.updateCine(cine)
-                } else {
-                    cineDao.createCine(
-                        Cine(
-                            name.text.toString(),
-                            address.text.toString(),
-                            website.text.toString(),
-                            latitudeStr.toDouble(),
-                            longitudeStr.toDouble(),
-                            image.text.toString()
-                        )
-                    )
-                }
 
-                Snackbar.make(v, "Cine ${name.text} saved!", Snackbar.LENGTH_LONG).show()
-                name.text.clear()
-                address.text.clear()
-                website.text.clear()
-                latitude.text.clear()
-                longitude.text.clear()
-                image.text.clear()
-
-                val action = CreateFragmentDirections.actionCineCreateToCineList()
-                v.findNavController().navigate(action)
+                val cineNew = Cine(
+                    name.text.toString(),
+                    address.text.toString(),
+                    website.text.toString(),
+                    GeoPoint(latitudeStr.toDouble(), longitudeStr.toDouble()),
+                    image.tag?.toString()
+                )
+                cloudDb.collection("cines").add(cineNew)
+                //cineDao.createCine(cineNew)
             }
+
+            Snackbar.make(v, "Cine ${name.text} saved!", Snackbar.LENGTH_LONG).show()
+            name.text.clear()
+            address.text.clear()
+            website.text.clear()
+            latitude.text.clear()
+            longitude.text.clear()
+            image.tag = ""
+
+            val action = CreateFragmentDirections.actionCineCreateToCineList()
+            v.findNavController().navigate(action)
         }
+    }
+
+    private fun checkFields(): Boolean {
+        return isComplete(name, getString(R.string.incomplete_name)) && (
+                isComplete(
+                    address,
+                    getString(R.string.incomplete_address)
+                ) || (
+                        isComplete(
+                            latitude,
+                            getString(R.string.incomplete_coordinates)
+                        ) &&
+                                isComplete(
+                                    longitude,
+                                    getString(R.string.incomplete_coordinates)
+                                )
+                        )
+                )
     }
 
     private fun isComplete(anEditText: EditText, anString: String): Boolean {
@@ -132,5 +209,44 @@ class CreateFragment : Fragment() {
             return false
         }
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image.setImageBitmap(imageBitmap)
+            image.setBackgroundResource(android.R.color.transparent)
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+        // Create a storage reference from our app
+        val storageRef = Firebase.storage.reference.child(
+            "images/${Timestamp(System.currentTimeMillis())}_${
+                UUID.randomUUID().toString().substring(0, 2)
+            }.jpg"
+        )
+        image.isDrawingCacheEnabled = true
+        image.buildDrawingCache()
+        val bitmap = (image.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = storageRef.putBytes(data)
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT)
+                    .show()
+                returnTransition
+            }
+            storageRef.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                image.tag = task.result;
+                image.setImageBitmap(bitmap)
+            }
+        }
     }
 }
